7616caed99d54a98937ef0c27f8165751a98904c
Michal Gralec <gralecmik9@gmail.com>
2021-03-14 19:24:13 +0100

    Log in page tests

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import unittest
 from selenium import webdriver
 from Pages import page
 
+
 class LogIn(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome("C:\Users\grale\Desktop\Projects\chromedriver_win32\chromedriver.exe")
+        self.driver = webdriver.Chrome(r"C:\Users\grale\Desktop\Projects\chromedriver_win32\chromedriver.exe")
         self.driver.get("https://www.saucedemo.com/")
+
+    def test_title(self):
+        loginPage = page.LogInPage()
+        assert loginPage.is_title_matches()
+
     def tearDown(self):
         self.driver.close()
